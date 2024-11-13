d555b9e5c65283e207c69a467428c9097cf581e1
HarshGupta <harshgupta88911@gmail.com>
2024-11-13 13:42:43 +0530

    update: href link form anchor tag

--- portfolio/components/navbar/navbar.jsx
+++ portfolio/components/navbar/navbar.jsx
@@ -2,22 +2,22 @@ import "@/style/home.css";
 
 function Navbar() {
   return (
-    <div className="nav_bar w-full items-center justify-between px-2 sm:flex">
+    <div className="items-center justify-between w-full px-2 nav_bar sm:flex">
       <span className="portfolio">Portfolio.</span>
       <div className="option-container">
-        <a href="#section-1" className="nav_links">
+        <a href="#Home-page" className="nav_links">
           Home
         </a>
-        <a href="#section-2" className="nav_links">
+        <a href="#Service-page" className="nav_links">
           Services
         </a>
-        <a href="#section-3" className="nav_links">
+        <a href="#Resume-page" className="nav_links">
           Resume
         </a>
-        <a href="#section-4" className="nav_links">
+        <a href="#Project-page" className="nav_links">
           Projects
         </a>
-        <a href="#section-5" className="nav_links">
+        <a href="#Contact-page" className="nav_links">
           Contact
         </a>
       </div>
